d7e1c8beffda7d99430d6b333df0b0129dc71fd0
explora <luis.monsalve@parqueexplora.org>
2020-09-30 15:14:47 -0500

    Versión final

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -3,15 +3,15 @@ const controller = {};
 controller.obtainMetaTags = (req, res) => {
     var id = req.query.id;
     if (!id) {
-        id = 1;
+        id = 1601477277811; //Id de la imagen por defecto
     }
 
     const data = {
         id,
-        title: "Título de prueba",
-        description: 'Descripción de prueba',
-        url: "https://fiestadellibroylacultura.com/",
-        image: 'https://imagenes-generadas.s3.amazonaws.com/' + id,
+        title: "Título", //Aquí va el título de la entrada
+        description: 'Descripción', //Aquí va la descripción
+        url: "https://fiestadellibroylacultura.com/", // Url final a donde redirige despues de abrir
+        image: 'https://imagenes-generadas.s3.amazonaws.com/' + id, //Link del servidor donde se esta guradando las imagenes que se van a compartir
     }
     res.render('template', {
         data,
